380ccd97c604db1200d054954eeb5d97f4eb49a3
winsky <1195413185@qq.com>
2019-02-24 17:46:10 +0800

    更新自启动脚本

--- auto_start.sh
+++ auto_start.sh
@@ -6,8 +6,11 @@ cd /home/blog
 # webhook服务启动
 /sbin/runuser -l root -c "pm2 start /home/blog/blog_webhooks.js"
 
+
+# 改为静态部署之后，不需要在自启动脚本中启动
+
 # 生成静态内容
-hexo generate && gulp
+# hexo generate && gulp
 
 # 启动博客
-hexo s &
+# hexo s &
