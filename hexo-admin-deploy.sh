bc5852bf68fd0552092d43e972e552fe643b7e0f
winsky <1195413185@qq.com>
2019-12-08 18:01:53 +0800

    admin部署脚本

--- hexo-admin-deploy.sh
+++ hexo-admin-deploy.sh
@@ -1,14 +1,11 @@
+msg=$1
+
 echo "提交内容： $msg"
 
-# 本地编译
-cd /home/blogSrc/
-echo '开始部署'
-hexo clean && hexo generate && hexo deploy 
-echo '部署完毕'
-
 # 提交到GitHub
+cd /home/blogSrc/
 git add . 
-git commit -m $1
+git commit -m $msg
 git push origin dev
 
 echo '成功推送到GitHub'
